b631b35a3a02f250134f6a4d8626df96f0b551a5
AbhishekTyagi0 <abhishektyagigi4310@gmail.com>
2023-11-18 21:57:25 +0530

    added structure of the cv builder app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
 import './App.css'
+import Input from "./components/Input";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function Heading(){
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>CV Builder</h1>
+      <div className="mainContainer">
+      <div className="userInput">
+        <Input />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="userOutput"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer><h2>Copyrigth &copy; All rights reserved</h2></footer>
     </>
   )
 }
 
-export default App
---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Input(){
+
+return (
+
+    <>
+    <h2 className="personalDetailHeading">Personal Details</h2>
+
+    
+    </>
+)
+
+}
